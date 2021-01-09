de4d0b26701324022a8d5e8f35a449629622b045
Phone Thiha Kyaw <mlsdphonethk@gmail.com>
2021-01-09 22:03:49 +0630

    Remove ipynb-checkpoints

--- checkers/.ipynb_checkpoints/Piece-checkpoint.py
+++
@@ -1,8 +0,0 @@
-class Piece:
-
-    def __init__(self, row, col, player):
-
-        self.position = (row, col)
-        self.player = player
-        self.selected = False
-        self.valid_grids = []
